c16f36a74d919bddfc51dd9880a544b66ae8dc07
DamiJJJ <damipl1280@outlook.com>
2023-02-23 16:02:51 +0100

    PlayerPrefs fixes

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,7 +5,15 @@ public class MainMenu : MonoBehaviour
 {
     private void Start()
     {
+        if(!PlayerPrefs.HasKey("snakeSpeed"))
+        {
+            PlayerPrefs.SetFloat("snakeSpeed", 15);          
+        }
         Snake.speed = PlayerPrefs.GetFloat("snakeSpeed");
+        if(!PlayerPrefs.HasKey("musicVolume"))
+        {
+            PlayerPrefs.SetFloat("musicVolume", 1);
+        }
         AudioListener.volume = PlayerPrefs.GetFloat("musicVolume");
     }
     public void QuitGame()
